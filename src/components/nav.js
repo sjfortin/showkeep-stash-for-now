fd8b5f60190d4ae9baf58527e94a9215a8794c24
Sam Fortin <sam.j.fortin@gmail.com>
2018-02-21 21:27:23 -0600

    playing with css frameworks. aka procrastinating

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Navigation = () => (
+  <AppBar
+    title="showkeep"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    style={{backgroundColor: 'darkgray'}}
+  />
+);
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bulma/css/bulma.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
